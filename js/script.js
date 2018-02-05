a37edea98e668d803b8855f8b7702f04466b4485
Youri Hdx <yourihdx@gmail.com>
2018-02-05 22:24:20 +0800

    added transition effect to buttons when removing class active

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $('#buttons-block').find('.twitch-link').on('click', function () {
      if ($(this).hasClass('active')) {
          return;
      }
-
+     
      $('#buttons-block').find('.twitch-link.active').removeClass('active');
 
      $(this).addClass('active');
