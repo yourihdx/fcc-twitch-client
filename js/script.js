f898d20e5db872d0f7d495dd5feb817c8e40f280
Youri Hdx <yourihdx@gmail.com>
2018-02-05 22:13:45 +0800

    when button clicked, it becomes active now, the others get inactive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('hello');
+
+$('#buttons-block').find('.twitch-link').on('click', function () {
+
+     if ($(this).hasClass('active')) {
+         return;
+     }
+
+     $('#buttons-block').find('.twitch-link.active').removeClass('active');
+
+     $(this).addClass('active');
+});
