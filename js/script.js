ca6f3ff647f1b462f9209c595980680789165ad7
Youri Hdx <yourihdx@gmail.com>
2018-02-12 22:31:23 +0800

    made a gamer_card var split to strings

--- js/script.js
+++ js/script.js
@@ -23,15 +23,17 @@ $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/RobotCaleb?callback=?',
   console.log('RobotCaleb: ', data.stream);
 });
 
-var gamer_card = `<a href="#">
-		<div class="card border-light text-center">		
-		  <div class="card-body">
-		  	<img class="rounded float-left" src="http://placeimg.com/80/80/tech/sepia" alt="Card image cap">
-		    <h5 class="card-title">ESL_SC2</h5>
-		    <p class="card-text">Supporting text for a streamer's content.</p>
-		  </div>
-		</div>
-		</a>`;
+// http://placeimg.com/80/80/tech/sepia
+
+var gamer_card = "<a href=\"#\">";
+gamer_card += "<div class=\"card border-light text-center\">";		
+gamer_card += "<div class=\"card-body\">";
+gamer_card += "<img class=\"rounded float-left\" src=\"http://placeimg.com/80/80/tech/sepia\" alt=\"Card image cap\">";
+gamer_card += "<h5 class=\"card-title\">{1}</h5>";
+gamer_card += "<p class=\"card-text\">Supporting text for a streamer's content.</p>";
+gamer_card += "</div></div></a>";
+
+ 		
 
 $("#main").html(gamer_card);
 
