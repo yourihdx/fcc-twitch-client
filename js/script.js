c149b2fe43c337b2ab1cab606dc11e2504a00ed1
Youri Hdx <yourihdx@gmail.com>
2018-02-13 10:34:26 +0800

    output gamer cards to the html

--- js/script.js
+++ js/script.js
@@ -14,16 +14,29 @@ $('#buttons-block').find('.twitch-link').on('click', function () {
 // https://wind-bow.glitch.me/twitch-api 
 function get_streamer_data(nickname='ESL_SC2'){
 	$.getJSON('https://wind-bow.glitch.me/twitch-api/streams/' + nickname +'?callback=?', function(data) {
-	  // $('.card-text').text(data);
 	  if (data.stream != null) {
 	  	var game = data.stream.game;
 	  	var status = data.stream.channel.status;
-	  	console.log(nickname, data.stream, '\n\n\n', game, status);
-	  }
+	  	var link = 'https://www.twitch.tv/' + nickname;
+	  	// console.log(nickname, game, status);
 	  
-	  else {
-	  	console.log(nickname, 'offline');
 	  }
+	  else {
+	  	// console.log(nickname, 'offline'); 
+	  	var game = '';
+	  	var status = 'offline';
+	  }
+
+  	  	var gamer_card = "<a href=\"#\">";
+		gamer_card += "<div class=\"card border-light text-center\">";		
+		gamer_card += "<div class=\"card-body\">";
+		gamer_card += "<img class=\"rounded float-left\" src=\"http://placeimg.com/80/80/tech/sepia\" alt=\"Card image cap\">";
+		gamer_card += "<h5 class=\"card-title\">" + nickname + "</h5>";
+		gamer_card += "<p class=\"card-text\">" + game + ":" + status + "</p>";
+		gamer_card += "</div></div></a>";
+
+		$(gamer_card).appendTo('#main');
+
 	});
 	}
 
@@ -31,12 +44,6 @@ var gamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", 
 for (var i = gamers.length - 1; i >= 0; i--) {
 	get_streamer_data(gamers[i]);
 }
-
-
-$.getJSON('https://wind-bow.glitch.me/twitch-api/streams/RobotCaleb?callback=?', function(data) {
-  // $('.card-text').text(data);
-  console.log('RobotCaleb: ', data.stream);
-});
 
 // http://placeimg.com/80/80/tech/sepia
 
@@ -50,5 +57,5 @@ gamer_card += "</div></div></a>";
 
  		
 
-$("#main").html(gamer_card);
+// $("#main").html(gamer_card);
 
